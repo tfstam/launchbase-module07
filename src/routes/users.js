503cf7c734415acfa571853fea495fe60b8f4005
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-26 15:50:08 -0300

    Rotas e Controllers de usuários

---
+++ src/routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// LOGIN/LOGOUT
+// routes.get('/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('/logout', SessionController.logout)
+// RESET PASSWORD / FORGOT
+// routes.get('/forgot-password', SessionController.forgotForm)
+// routes.get('/password-reset', SessionController.resetForm)
+// routes.post('/forgot-password', SessionController.forgot)
+// routes.post('/password-reset', SessionController.reset)
+
+// USER REGISTER UserController
+// routes.get('/register', UserController.registerForm)
+// routes.post('/register', UserController.post)
+
+// routes.get('/', UserController.show)
+// routes.put('/', UserController.update)
+// routes.delete('/', UserController.delete)
+
+module.exports = routes
