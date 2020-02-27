7c3fd4f11b09f2f511d8f4e92c2fdf66f3da779d
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-27 10:59:31 -0300

    Estrutura de validação de email

--- public/scripts.js
+++ public/scripts.js
@@ -181,3 +181,20 @@ const Lightbox = {
     Lightbox.closeButton.style.top = '-80px'
   }
 }
+
+const Validate = {
+  apply(input, func) {
+    let results = Validate[func](input.value)
+    input.value = results.value
+
+    if (results.error) 
+      alert('ERROOOUU!!')
+  },
+  isEmail(value) {
+    let error = null
+    return {
+      error,
+      value
+    }
+  }
+}
